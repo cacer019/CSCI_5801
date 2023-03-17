5254a92a0a2e5940cb813b22880d3ea21986b85e
tracy255 <tracy255@umn.edu>
2023-03-17 17:47:57 -0500

    Initial Class Setup
    Nothing done besides class names

---
+++ Project1/src/main/java/IRProcessing.java
@@ -0,0 +1,2 @@
+public class IRProcessing {
+}
--- Project1/src/testing/java/TestClass.java
+++ Project1/src/testing/java/TestClass.java
@@ -1,7 +1,6 @@
 package com.vogella.junit.first;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 class AClassWithOneJUnitTest {

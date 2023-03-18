0140953a75a5e9ac9f5a6c79d96e24a1419fc085
Garrett <abouz009@umn.edu>
2023-03-18 17:59:32 -0500

    Update Party.java

--- Project1/src/main/java/Party.java
+++ Project1/src/main/java/Party.java
@@ -9,14 +9,28 @@ public class Party implements IRepresentative {
     };
 
     public void addCandidate(String candidateName) {
-
+        int n = candidates.length;
+        String[] newCandidates = new String[n+1];
+        for (int i = 0; i < n; i++) {
+            newCandidates[i] = candidates[i];
+        }
+        newCandidates[n] = candidateName;
+        candidates = newCandidates
     }
 
     public void removeCandidate(String candidateToRemove) {
-
+        int n = candidates.lengh;
+        String newCandidates[] = new String[n - 1];
+        for (int i = 0, k = 0; i < arr.length; i++) {
+            if (candidates[i] != candidateToRemove) {
+                newCandidates[k] = candidates[i];
+                k++;
+            }
+        }
+        candidates = newCandidates;
     }
+
     public String[] getCandidate() {
-
+        return candidates;
     }
-
 }

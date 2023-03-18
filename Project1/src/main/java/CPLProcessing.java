20e31f89ddeaa54a2a5c05324c9199a3faa446bb
Garrett <abouz009@umn.edu>
2023-03-18 16:19:38 -0500

    Created template for Party and CPLProcessing classes

--- Project1/src/main/java/CPLProcessing.java
+++ Project1/src/main/java/CPLProcessing.java
@@ -1,2 +1,28 @@
-public class CPLProcessing {
+public class CPLProcessing implements IBallotProcessing {
+
+    private Party[] parties;
+
+    private void distributeSeats(String filename) {
+
+    }
+
+    private void setParty(String[] something) {
+
+    }
+
+    private void distributeBallots(String fileName, party[] parties) {
+
+    }
+
+    public boolean processElection(String fileName, ProcessResults results) {
+
+    }
+
+    public String[] getCandidates() {
+
+    }
+
+    public String[] getParties() {
+
+    }
 }
--- Project1/src/main/java/Party.java
+++ Project1/src/main/java/Party.java
@@ -1,2 +1,22 @@
-public class Party {
+public class Party implements IRepresentative {
+
+    private String[] candidates;
+    private int numSeats;
+
+    public void Party(String party, String[] candidateList) {
+        partyName = party;
+        candidates = candidateList;
+    };
+
+    public void addCandidate(String candidateName) {
+
+    }
+
+    public void removeCandidate(String candidateToRemove) {
+
+    }
+    public String[] getCandidate() {
+
+    }
+
 }

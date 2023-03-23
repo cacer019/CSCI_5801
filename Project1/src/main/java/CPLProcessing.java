38a7a6f486cf34552ede9ae1b2cb7a4d0eaed00d
Garrett <abouz009@umn.edu>
2023-03-22 22:59:30 -0500

    Created setParties method

--- Project1/src/main/java/CPLProcessing.java
+++ Project1/src/main/java/CPLProcessing.java
@@ -1,15 +1,31 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CPLProcessing implements IElectionProcessing {
 
-    private Party[] parties;
+    private ArrayList<Party> parties;
 
     private void distributeSeats(BufferedReader br) {
 
     }
 
-    private void setParty(String[] something) {
-
+    private void setParties(BufferedReader br) throws IOException {
+        ArrayList<Party> listOfParties = new ArrayList<>();
+        // Gets number if parties (2nd Line)
+        int numParties = Integer.parseInt(br.readLine());
+        // Gets list of party names (3rd line)
+        String curLine = br.readLine();
+        String[] partyNames = curLine.split(",");
+        // Reads candidates of each party and creates a Party object with respective party name and candidates
+        for (int i = 0; i < numParties; i++) {
+            curLine = br.readLine();
+            ArrayList<String> candidatesOfParty = new ArrayList<>(Arrays.asList(curLine.split(";")));
+            Party party = new Party(partyNames[i], candidatesOfParty);
+            listOfParties.add(party);
+        }
+        this.parties = listOfParties;
     }
 
     private void distributeBallots(BufferedReader br, Party[] parties) {
